ce4e7e50bd58cecca0e5d951e1adae43e44d2d18
JoeSteeloak <141022479+JoeSteeloak@users.noreply.github.com>
2024-05-20 10:51:23 +0200

    built all components

---
+++ src/app/pages/overall-plan/overall-plan.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-overall-plan',
+  standalone: true,
+  imports: [],
+  templateUrl: './overall-plan.component.html',
+  styleUrl: './overall-plan.component.scss'
+})
+export class OverallPlanComponent {
+
+}
