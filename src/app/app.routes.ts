4e61785223f841a8848f47d06e049e709f1fcd87
JoeSteeloak <141022479+JoeSteeloak@users.noreply.github.com>
2024-05-22 14:10:00 +0200

    start styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { CoursesComponent } from './pages/courses/courses.component';
 import { OverallPlanComponent } from './pages/overall-plan/overall-plan.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
+    { path: "home", component: HomeComponent },
     { path: "courses", component: CoursesComponent },
-    { path: "", redirectTo: "/courses", pathMatch: "full"},
+    { path: "", redirectTo: "/home", pathMatch: "full"},
     { path: "overall-plan", component: OverallPlanComponent}
 ];
