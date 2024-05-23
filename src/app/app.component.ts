e9bbd246b44e10f38ed0ec2e097c2b739770fbd0
JoeSteeloak <141022479+JoeSteeloak@users.noreply.github.com>
2024-05-23 16:42:23 +0200

    add dark theme toggle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { DarkToggleComponent } from './dark-toggle/dark-toggle.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, NavbarComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, NavbarComponent, DarkToggleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
